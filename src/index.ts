a126137638205cd6fc5d693b7601f013a341a779
geoskk <16525346+geoskk@users.noreply.github.com>
2021-03-14 15:46:19 +0530

    Update index.ts
    server.use(cors()); added before router

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const express = require('express');
 const server = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
+server.use(cors());
 
 router(server);
 
@@ -14,7 +15,7 @@ server.get('/', function (req: any, res: any) {
 });
 server.use('/', express.static(publicFolder));
 
-server.use(cors());
+
 
 server.listen(port, () => {
 
